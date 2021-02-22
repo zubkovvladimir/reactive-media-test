c6cdf029cdbbaec611d57363221c0a673aa3377a
zubkovvladimir <njoy70@mail.ru>
2021-02-22 05:39:07 +0500

    feat: backup

---
+++ source/js/main.js
@@ -0,0 +1,117 @@
+'use strict';
+
+(function () {
+  var form = document.querySelector('.form');
+
+  var firstStep = form.querySelector('.declarant');
+  var secondStep = form.querySelector('.details');
+  var thirdStep = form.querySelector('.agreement');
+
+  var firstButton = form.querySelector('.declarant .btn');
+  var secondButton = form.querySelector('.details .btn');
+
+  var declarantWrap = form.querySelector('.declarant__wrap');
+  var detailsWrap = form.querySelector('.details__wrap');
+  var agreementWrap = form.querySelector('.agreement__wrap');
+
+  var popup = document.querySelector('.message-succes');
+
+  var hideThirdStep = function () {
+    thirdStep.style.height = '80px';
+    agreementWrap.style.display = 'none';
+    thirdStep.style.backgroundImage = 'url("../../img/third-gray.svg")';
+  }
+
+  var setStylesStep = function (resizeBlock, hideBlock, showBlock, backgroundBlock, stepNumber) {
+    resizeBlock.style.height = '80px';
+    showBlock.style.display = 'flex';
+    hideBlock.style.display = 'none';
+    stepNumber === 'first' ? resizeBlock.style.backgroundImage = 'url("../../img/second-gray.svg")' : resizeBlock.style.backgroundImage = 'url("../../img/agree-icon.svg")';;
+    backgroundBlock.style.backgroundImage = 'url("../../img/' + stepNumber + '-blue.svg")';
+  }
+
+  var onFirstStepClick = function () {
+    var start = Date.now();
+
+    setStylesStep(secondStep, detailsWrap, declarantWrap, firstStep, 'first');
+    hideThirdStep();
+
+    var timer = setInterval(function () {
+      var timePassed = (Date.now() - start) * 3;
+
+      if (timePassed > 80) {
+        firstStep.style.height = timePassed + 'px';
+      }
+
+      if (timePassed > 344) {
+        firstStep.style.height = '344px';
+        clearInterval(timer);
+
+        firstStep.removeEventListener('click', onFirstStepClick);
+        secondStep.removeEventListener('click', onSecondStepClick);
+      };
+
+    }, 20);
+  }
+
+  var onFirstButtonClick = function () {
+    var start = Date.now();
+
+    setStylesStep(firstStep, declarantWrap, detailsWrap, secondStep, 'second');
+
+    var timer = setInterval(function () {
+      var timePassed = (Date.now() - start) * 3;
+
+      if (timePassed > 80) {
+        secondStep.style.height = timePassed + 'px';
+      }
+
+      if (timePassed > 992) {
+        secondStep.style.height = '992px';
+        clearInterval(timer);
+
+        firstStep.addEventListener('click', onFirstStepClick);
+      };
+
+    }, 20);
+  }
+
+  var onSecondStepClick = function () {
+    onFirstButtonClick();
+    secondStep.removeEventListener('click', onSecondStepClick);
+    hideThirdStep();
+  }
+
+  var onSecondButtonClick =  function () {
+    var start = Date.now();
+
+    setStylesStep(secondStep, detailsWrap, agreementWrap, thirdStep, 'third');
+
+    var timer = setInterval(function () {
+      var timePassed = (Date.now() - start) * 3;
+
+      if (timePassed > 80) {
+        thirdStep.style.height = timePassed + 'px';
+      }
+
+      if (timePassed > 846) {
+        thirdStep.style.height = '855px';
+        clearInterval(timer);
+
+        secondStep.addEventListener('click', onSecondStepClick);
+      };
+
+    }, 20);
+  }
+
+
+  firstButton.addEventListener('click', onFirstButtonClick);
+  secondButton.addEventListener('click', onSecondButtonClick);
+
+  form.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+
+    form.style.display = 'none';
+    popup.style.display = 'block';
+  });
+})();
---
+++ source/js/validation.js
@@ -0,0 +1,107 @@
+// 'use strict';
+
+// (function () {
+//   var HASHTAG_PATTERN = /.[^a-zA-Zа-яА-Я0-9]/g;
+//   var IS_HASH_SYMBOL = /[^#]/g;
+//   var IS_SPACE = /.#/g;
+//   var HASHTAG_MAX_LENGTH = 21;
+//   var HASHTAG_MAX_COUNT = 5;
+
+//   var picturesContainer = document.querySelector('.pictures');
+//   var form = picturesContainer.querySelector('.img-upload__form');
+//   var formUploadHashtags = form.querySelector('.text__hashtags');
+
+//   // создает метод для валидации хештегов в форме редактирования
+
+//   var CustomValidation = function () {
+//     this.invalidities = [];
+//     this.validityChecks = [];
+//     this.uniqueInvalidities = [];
+//   };
+
+//   // опеределяет прототип метода валидации
+
+//   CustomValidation.prototype = {
+//     addInvalidity: function (message) {
+//       this.invalidities.push(message);
+//     },
+//     getInvalidities: function () {
+//       this.uniqueInvalidities = [...new Set(this.invalidities)];
+
+//       return this.uniqueInvalidities.join('. \n');
+//     },
+//     checkValidity: function (input, arrayHashtags) {
+//       var isInvalid = this.validityChecks.isInvalid(input, arrayHashtags);
+
+//       if (isInvalid) {
+//         this.addInvalidity(this.validityChecks.invalidityMessage);
+//         this.validityChecks.element.classList.add('invalid');
+//         this.validityChecks.element.classList.remove('valid');
+//       } else {
+//         this.validityChecks.element.classList.remove('invalid');
+//         this.validityChecks.element.classList.add('valid');
+//         }
+//     }
+//   };
+
+//   // массив проверок валидации
+
+//   var usernameValidityChecks = [
+//     {
+//       isInvalid: function (input) {
+//         return input !== '';
+//       },
+//       invalidityMessage: 'Заполните это поле.',
+//       element: form.querySelector('.input-requirements li:nth-child(1)')
+//     }
+//   ];
+
+//   // валидирует поле хештегов в реальном времени
+
+//   var checkValidity = function (evt) {
+//     formUploadHashtags.CustomValidation.invalidities = [];
+//     var arrayHashtags = evt.target.value.split(' ');
+
+//     for (var i = 0; i < arrayHashtags.length; i++) {
+//       formUploadHashtags.CustomValidation.checkValidity(arrayHashtags[i], arrayHashtags);
+//     }
+
+//     var isEmpty = formUploadHashtags.CustomValidation.invalidities.length === 0
+//                                             && formUploadHashtags.value !== '';
+
+//     if (isEmpty) {
+//       formUploadHashtags.setCustomValidity('');
+//     } else if (arrayHashtags.length === 1 && arrayHashtags[0] === ''){
+//       formUploadHashtags.setCustomValidity('');
+//     } else {
+//       var message = formUploadHashtags.CustomValidation.getInvalidities();
+//       formUploadHashtags.setCustomValidity(message);
+//     }
+//   };
+
+//   formUploadHashtags.CustomValidation = new CustomValidation();
+//   formUploadHashtags.CustomValidation.validityChecks = usernameValidityChecks;
+
+//   window.validation = {
+//     onTextHashtagsInput: checkValidity
+//   }
+// })();
+
+
+// (function () {
+//   var CustomValidation = function () {
+//     this.invalidities = [];
+//   }
+
+//   CustomValidation.prototype = {
+//     addInvalidity: function (message) {
+//       this.invalidities.push(message);
+//     },
+//     getInvalidities: function () {
+//       return this.invalidities.join('. \n');
+//     },
+//     checkValidity: function () {
+
+//     }
+//   }
+// })();
