f657eb391272bda57e4f669fc4b993086284faa6
zubkovvladimir <njoy70@mail.ru>
2021-03-09 15:27:58 +0500

    fix: js by eslint

--- source/js/main.js
+++ source/js/main.js
@@ -28,33 +28,28 @@ const message = document.querySelector('.message-succes');
 // validation second step
 
 const checkInputsValidation = function (inputs) {
-  console.log(inputs);
   let errorsCount = 0;
   const isChecbox = inputs[0].type === 'checkbox';
-  console.log(isChecbox + ' isChekbox');
 
   inputs.forEach(input => {
     const isEmpty = isChecbox ? !(input.checked) : input.value === '';
-    console.log(input);
-    console.log(isEmpty);
 
     if (isEmpty) {
       isChecbox ? input.labels[0].style.borderColor = 'red' : input.style.borderColor = 'red';
       errorsCount += 1;
     } else if (input.value !== '') {
       isChecbox ? input.labels[0].style.borderColor = '' : input.style.borderColor = '';
-    };
+    }
   });
-  console.log(errorsCount);
   return errorsCount;
-}
+};
 
-$("#tel").mask("+7 (999) 999-99-99");
-$("#series").mask("9999");
-$("#number").mask("999999");
-$("#day").mask("99");
-$("#month").mask("99");
-$("#year").mask("9999");
+$('#tel').mask('+7 (999) 999-99-99');
+$('#series').mask('9999');
+$('#number').mask('999999');
+$('#day').mask('99');
+$('#month').mask('99');
+$('#year').mask('9999');
 
 /* end validation */
 
@@ -62,16 +57,16 @@ const hideThirdStep = function () {
   thirdStep.style.height = MIN_STEP_HEIGHT + 'px';
   agreementWrap.style.display = 'none';
   thirdStep.style.backgroundImage = 'url("./img/third-gray.svg")';
-}
+};
 
 const setStylesStep = function (resizeBlock, hideBlock, showBlock, backgroundBlock, stepNumber) {
   resizeBlock.style.height = MIN_STEP_HEIGHT + 'px';
   showBlock.style.display = 'flex';
   hideBlock.style.display = 'none';
   stepNumber === IS_FIRST_STEP ? resizeBlock.style.backgroundImage = 'url("./img/second-gray.svg")'
-                                : resizeBlock.style.backgroundImage = 'url("./img/agree-icon.svg")';
+    : resizeBlock.style.backgroundImage = 'url("./img/agree-icon.svg")';
   backgroundBlock.style.backgroundImage = 'url("./img/' + stepNumber + '-blue.svg")';
-}
+};
 
 const onFirstStepClick = function () {
   const start = Date.now();
@@ -92,10 +87,9 @@ const onFirstStepClick = function () {
 
       firstStep.removeEventListener('click', onFirstStepClick);
       secondStep.removeEventListener('click', onSecondStepClick);
-    };
-
+    }
   }, INTERVAL_DELAY);
-}
+};
 
 const onFirstButtonClick = function () {
   const start = Date.now();
@@ -115,17 +109,16 @@ const onFirstButtonClick = function () {
       hideThirdStep();
 
       firstStep.addEventListener('click', onFirstStepClick);
-      secondButton.addEventListener('click',onSecondButtonClick);
-      form.addEventListener('change',onFormChange);
-    };
-
+      secondButton.addEventListener('click', onSecondButtonClick);
+      form.addEventListener('change', onFormChange);
+    }
   }, INTERVAL_DELAY);
-}
+};
 
 const onSecondStepClick = function () {
   onFirstButtonClick();
   secondStep.removeEventListener('click', onSecondStepClick);
-}
+};
 
 const showThirdStep = function () {
   const start = Date.now();
@@ -145,29 +138,28 @@ const showThirdStep = function () {
 
       secondStep.addEventListener('click', onSecondStepClick);
       form.addEventListener('submit', onFormSubmit);
-    };
-
+    }
   }, INTERVAL_DELAY);
-}
+};
 
-const onSecondButtonClick =  function () {
+const onSecondButtonClick = function () {
   const inputs = detailsWrap.querySelectorAll('input');
   const isErrors = checkInputsValidation(inputs);
 
   if (!isErrors) {
     showThirdStep();
   }
-}
+};
 
 const onFormChange = function () {
   const inputs = detailsWrap.querySelectorAll('input');
   const isErrors = checkInputsValidation(inputs);
 
   if (!isErrors) {
-    form.removeEventListener('change',onFormChange);
-    secondButton.addEventListener('click',onSecondButtonClick);
+    form.removeEventListener('change', onFormChange);
+    secondButton.addEventListener('click', onSecondButtonClick);
   }
-}
+};
 
 const onFormSubmit = function (evt) {
   evt.preventDefault();
@@ -179,6 +171,5 @@ const onFormSubmit = function (evt) {
     message.style.display = 'block';
   }
 };
-
 
 firstButton.addEventListener('click', onFirstButtonClick);
